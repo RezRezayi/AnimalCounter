3b79c2082327c5f391ec15e7561eee6914634007
RezRezayi <rezvane.rezayi@hotmail.com>
2020-09-23 07:31:12 -0700

    Form Validation

--- src/container/login/Register.jsx
+++ src/container/login/Register.jsx
@@ -1,65 +1,141 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "./Register.css";
-
-class Register extends Component {
-    state = {}
-    render() {
-        return (
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import FormikControl from './FormikControl';
 
 
 
-<React.Fragment>
 
 
-<div className="modal-dialog text-center">
-    <div className="col-sm-8 main-section">
-        <div className="modal-content">
-        <h2>Sign Up</h2>
-        
-            <form action="" className="col-12">
-               
-                <div className="form-group list-group-item">
-                    <span className="login-icon"><i className="fa fa-user"></i>
-                    </span>
-                    <input type="text" className="form-control login-input" placeholder="Enter Your Name"/>
-                </div>
+const Register = () => {
 
-        
-    
+    const initialValues = {
 
-        <div className="form-group list-group-item">
-            <span className="login-icon"><i className="fa fa-envelope"></i>
-            </span>
-            <input type="email" className="form-control login-input" placeholder="Enter Your Email"/>
-        </div>
 
-        <div className="form-group list-group-item">
-            <span className="login-icon"><i className="fa fa-phone"></i>
-            </span>
-            <input type="text" className="form-control login-input" placeholder="Enter Your Animal's Age"/>
-        </div>
+        name: "",
+        email: "",
+        age: 0,
+        password: ""
 
-        <div className="form-group list-group-item">
-            <span className="login-icon"><i className="fa fa-lock"></i>
-            </span>
-            <input type="password" className="form-control login-input" placeholder="Enter Your Password"/>
-        </div>
-        <button type="submit" className="btn btn-primary"><i className="fas fa-sign-in-alt"></i>Submit</button>
-        </form>
-     
+    }
 
-    </div>
-</div>  
-</div>
 
-  
+    const validationSchema = Yup.object({
+
+        name: Yup.string()
+            .min(3, "Must be atleast 3 characters")
+            .max(15, "Must be less than 15 charecters")
+            .required("Required")
+        ,
+        email: Yup.string()
+            .email("Email Is Not Valid")
+            .required("Required"),
+
+
+        age: Yup.number()
+            .min(10, "Your Animal is too young"),
+
+        password: Yup.string()
+            .required('Please Enter your password')
+            .matches(
+                /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+            ),
 
 
 
-            </React.Fragment>
+    })
 
 
-                );
-           }
-       }
-       export default Register;
+    const onSubmit = values => {
+        console.log(values);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    return (
+
+
+        <Formik initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}>
+
+
+
+
+            {formik => {
+
+                return <Form>
+
+                    <FormikControl
+
+                        control="input"
+                        type='text'
+                        label="Name"
+                        name="name"
+
+                    />
+
+
+                    <FormikControl
+
+                        control="input"
+                        type='email'
+                        label="Email"
+                        name="email"
+
+                    />
+
+
+                    <FormikControl
+
+                        control="input"
+                        type='number'
+                        label="Age"
+                        name="age"
+
+                    />
+
+
+
+
+                    <FormikControl
+
+                        control="input"
+                        type='password'
+                        label="Password"
+                        name="password"
+
+                    />
+
+                    <button type="submit" disabled={!formik.isValid}>Join Us!</button>
+
+
+                </Form>
+
+            }}
+
+
+
+
+
+
+        </Formik>
+
+
+    );
+
+}
+
+export default Register;
+
+
